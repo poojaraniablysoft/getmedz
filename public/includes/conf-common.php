91958a965f3de308d73bfc2ad82faa400389df05
Pooja Rani <poojarani@4demo.biz>
2017-11-17 13:54:13 +0530

    Database Configuration File Updated

--- public/includes/conf-common.php
+++ public/includes/conf-common.php
@@ -9,20 +9,27 @@ define('CONF_LIB_HALDLE_ERROR_IN_PRODUCTION', true);
 
 define ('CONF_URL_REWRITING_ENABLED', true);
 define('CONF_WEBSITE_URL','/');
-if($_SERVER['SERVER_NAME']=='getmedz.4demo.biz'){
+if($_SERVER['SERVER_NAME']=='poojarani.4demo.biz'){
     define('CONF_WEBROOT_URL', '/');
 
     define('CONF_DB_SERVER', 'localhost');
     define('CONF_DB_USER', 'developer');
     define('CONF_DB_PASS', 'developer');
-    define('CONF_DB_NAME', 'dev_get_medz');
+    define('CONF_DB_NAME', 'dev_getmedz');
+}elseif($_SERVER['SERVER_NAME']=='staging.4demo.biz'){
+    define('CONF_WEBROOT_URL', '/');
+
+    define('CONF_DB_SERVER', 'localhost');
+    define('CONF_DB_USER', 'staging');
+    define('CONF_DB_PASS', 'staging');
+    define('CONF_DB_NAME', 'staging_getmedz');
 }
 else{
     define('CONF_WEBROOT_URL', '/');
     define('CONF_DB_SERVER', 'localhost');
     define('CONF_DB_USER', 'developer');
     define('CONF_DB_PASS', 'developer');
-    define('CONF_DB_NAME', '');
+    define('CONF_DB_NAME', 'dev_get_medz');
 }
 
 define('PASSWORD_SALT', 'ewoiruqojfklajreajflajoer');
